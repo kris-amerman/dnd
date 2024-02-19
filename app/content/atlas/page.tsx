78abebfc2cb8e9fc4e5462551779610a143aecac
Kristoffer Amerman <kristofferamerman@kris-mac.local>
2024-02-19 11:02:57 -0500

    remove leaflet, add mapbox map

--- app/content/atlas/page.tsx
+++ app/content/atlas/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 export default function Page() {
     return (
       <main>
-        <Map></Map>
+        <Map />
       </main>
     );
 }
--- app/ui/atlas/map.tsx
+++ app/ui/atlas/map.tsx
@@ -1,99 +1,200 @@
+/**
+ * @file Aberran globe map
+ * @author Kris Amerman <krisamerman@gmail.com>
+ * @version 0.0.1
+ * 
+ * @todo Open PR on DefinitelyTyped (type mismatch)
+ *   - Example: mapboxgl.MapboxOptions.projection expects Projection, but should be string
+ *   - types/mapbox-gl version -> v2.7.21
+ *   - mapbox - gl version -> v3.1.2
+ */
+
 'use client'
-import { useEffect, useRef } from 'react';
-import 'leaflet/dist/leaflet.css';
-import L, { LatLng } from 'leaflet';
-import { LatLngBoundsExpression } from 'leaflet';
-import { geoJSON } from 'leaflet';
-import { LatLngTuple } from 'leaflet';
 
+// ======================== Imports ========================
+import React, { useRef, useState, useEffect } from 'react';
+import "mapbox-gl/dist/mapbox-gl.css";
+import "@/app/ui/atlas/map.css"
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-const Map = () => {
-  const mapRef = useRef<L.Map | null>(null);
+//======================== Token init ========================
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jpc2FtZXJtYW4iLCJhIjoiY2xzaWRubWVlMmY1bTJscXU3Z2dyaXIydyJ9.Yo-Ih8j1RNGTnk7UJ4XpiQ';
+
+// ======================== Type definitions ========================
+
+type MyGeoJsonProperties = GeoJSON.GeoJsonProperties & { name: string }
+
+// ======================== Map component ========================
+
+export default function Map() {
+  const mapContainer = useRef(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+  const [lng, setLng] = useState(46.556);
+  const [lat, setLat] = useState(53.358);
+  const [zoom, setZoom] = useState(2);
 
   useEffect(() => {
-    if (!mapRef.current) {
-      // Center of the map 
-      const latitude = 40.725374205068128;
-      const longitude = -73.960974979866151;
+    if (map.current) return; // initialize only once
 
-      // Zoom level
-      const zoomLevel = 15;
+    // Initialize map
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current!,
+      projection: 'globe' as any,
+      // style: "mapbox://styles/krisamerman/clspe70h804dv01qs1m1ebovv/draft",
+      style: {
+        version: 8,
+        sources: {},
+        layers: [
+          {
+            id: 'background',
+            type: 'background',
+            paint: { 'background-color': '#d9c797' }
+          }
+        ]
+      },
+      center: [lng, lat],
+      zoom: zoom,
+      pitch: 90,
+      bearing: 0,
+      antialias: true,
+    });
 
-      // Bounds of the image
-      const south = 40.7000;
-      const west = -74.0200;
-      const north = 40.7300;
-      const east = -73.9800;
+    // When map moves, update coords state
+    map.current.on('move', () => {
+      setLng(Number(map.current!.getCenter().lng.toFixed(4)));
+      setLat(Number(map.current!.getCenter().lat.toFixed(4)));
+      setZoom(Number(map.current!.getZoom().toFixed(2)));
+    });
 
-
-      // Image dimensions
-      const imageWidth = 4096;
-      const imageHeight = 1969;
-
-      // Calculate aspect ratio
-      const aspectRatio = imageWidth / imageHeight;
-
-      // Calculate overlay size to maintain aspect ratio
-      const overlayWidth = 0.1; // Adjust this value based on your preference
-      const overlayHeight = overlayWidth / aspectRatio;
-
-      // Example map image
-      const imageUrl = '/map.jpg';
-
-      // Calculate the new bounds to maintain aspect ratio
-      const boundsWidth = overlayWidth / 2; // Adjust this value based on your preference
-      const boundsHeight = boundsWidth / aspectRatio;
-      const newImageBounds = L.latLngBounds(
-        L.latLng(south + (overlayHeight - boundsHeight) / 2, west + (overlayWidth - boundsWidth) / 2),
-        L.latLng(south + (overlayHeight + boundsHeight) / 2, west + (overlayWidth + boundsWidth) / 2)
-      );
-
-      // Create the map instance
-      const map = L.map('map', {
-        center: [latitude, longitude],
-        zoom: zoomLevel,
-        maxBounds: newImageBounds, // Set maxBounds to the bounds of the image 
+    // Add atmosphere
+    map.current.on('style.load', () => {
+      map.current!.setFog({
+        color: 'rgb(186, 210, 235)',
+        'high-color': 'rgb(36, 92, 223)',
+        'horizon-blend': 0.02,
+        'space-color': 'rgb(11, 11, 25)',
+        'star-intensity': 0.6
       });
+    });
 
-      L.imageOverlay(imageUrl, newImageBounds, { interactive: true })
-        .addTo(map)
-        .setBounds(newImageBounds);
+    // Fetch GeoJSON and build polygons
+    const buildMapLayers = async () => {
+      let highlightId: number | null = null;
 
-      // const polygon = L.polygon([
-      //   [40.73606, -73.9950],
-      //   [40.73606, -73.9950],
-      //   [40.73606, -73.9950],
-      //   [40.73606, -73.9950]
-      // ], {color: 'red'}).addTo(map);
+      try {
+        // Fetch polygon data
+        const response = await fetch('/mapped_ids.geojson');
+        const geoJSONData: GeoJSON.FeatureCollection<
+          GeoJSON.Geometry,
+          MyGeoJsonProperties
+        > = await response.json();
 
-      const latlns: LatLngTuple[] = [[-73.960974979866151, 40.725374205068128], [-73.960806889354146, 40.725368201835558], [-73.960758863493581, 40.725386211533277], [-73.960722844098143, 40.725362198602987], [-73.960548750353567, 40.725380208300699], [-73.960506727725559, 40.725398217998418], [-73.960476711562706, 40.725380208300699], [-73.960362650143836, 40.725392214765847], [-73.960314624283271, 40.725422230928707], [-73.960290611352974, 40.725422230928707], [-73.960260595190121, 40.725404221230988], [-73.960206566096971, 40.72541622769613], [-73.96013452730611, 40.725380208300699], [-73.960116517608398, 40.725356195370416], [-73.960026469119825, 40.725362198602987], [-73.959972440026675, 40.725260143649265], [-73.959996452956972, 40.725248137184124], [-73.960020465887254, 40.725206114556123], [-73.960074494980404, 40.725188104858404], [-73.960080498212974, 40.72503802404411], [-73.960206566096971, 40.725014011113828], [-73.960218572562127, 40.724996001416109], [-73.960296614585559, 40.724947975555537], [-73.960338637213553, 40.724935969090396], [-73.960356646911265, 40.724977991718397], [-73.960440692167268, 40.724977991718397], [-73.960470708330135, 40.724935969090396], [-73.960506727725559, 40.724947975555537], [-73.96063279560957, 40.724863930299534], [-73.960644802074711, 40.724887943229824], [-73.960746857028425, 40.724941972322966], [-73.960806889354146, 40.724941972322966], [-73.960806889354146, 40.724971988485827], [-73.960860918447295, 40.724971988485827], [-73.960866921679866, 40.725014011113828], [-73.960836905517013, 40.72509205313726], [-73.960896937842719, 40.725140078997832], [-73.960914947540445, 40.725194108090982], [-73.960914947540445, 40.725248137184124], [-73.960956970168439, 40.725290159812126], [-73.960980983098736, 40.725332182440127], [-73.960974979866151, 40.725374205068128]].map((subArray) => [subArray[1], subArray[0]])
+        // Add polygons source to map
+        map.current!.addSource('custom-polygons', {
+          type: 'geojson',
+          data: geoJSONData,
+        });
 
-      const polygon2 = L.polygon(latlns, {color: 'red'}).addTo(map).bindPopup('A belligerent kingdom, long a thorn in the side of its neighbours, particularly its ancestral enemy- the Amaravati to the north');
+        // Add polygon borders
+        map.current!.addLayer({
+          id: 'custom-polygons-border',
+          type: 'line',
+          source: 'custom-polygons',
+          paint: {
+            'line-color': '#826439',
+            'line-width': 1,
+          },
+        });
 
+        // Add polygon fills
+        map.current!.addLayer({
+          id: 'custom-polygons-layer',
+          type: 'fill',
+          source: 'custom-polygons',
+          paint: {
+            'fill-color': '#a27d48',
+            'fill-opacity': [
+              'case',
+              ['boolean', ['feature-state', 'hover'], false],
+              0.95,
+              0.7
+            ],
+          }
+        });
 
-      // Load GeoJSON data
-      fetch('./app/ui/atlas/polygon.geojson')
-        .then(response => response.json())
-        .then(geoJsonData => {
-          console.log("HERE")
-          // Add GeoJSON data to the map
-          L.geoJSON(geoJsonData, {
-            style: {
-              color: 'green',  // Change the color as needed
-              weight: 2,
-              opacity: 1,
-            },
-          }).addTo(map);
-        })
-        .catch(error => console.error('Error loading GeoJSON:', error));
+        // ======================== Add mouse interactions ========================
 
+        // MOUSE CLICK
+        map.current!.on('click', 'custom-polygons-layer', (e) => {
+          const feature = e.features![0];
+          const { name } = feature.properties as MyGeoJsonProperties;
+          const description = "placeholder";
 
-      // Save the map instance to the ref
-      mapRef.current = map;
+          // Add popup to map at current lnglat
+          new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(`<div><h1>${name}</h1><p>${description}</p></div>`)
+            .addTo(map.current!);
+        });
+
+        // MOUSE MOVE
+        map.current!.on('mousemove', 'custom-polygons-layer', (e) => {
+
+          // Update cursor (clickable)
+          if (map.current!.getCanvas().style.cursor !== 'pointer') {
+            map.current!.getCanvas().style.cursor = 'pointer';
+          }
+
+          if (e.features && e.features.length > 0) {
+            if (e.features[0].id === highlightId) return; // ignore repeats
+
+            // If a polygon is already highlighted, un-highlight it 
+            if (highlightId !== null) {
+              map.current!.setFeatureState(
+                { source: 'custom-polygons', id: highlightId },
+                { hover: false }
+              );
+            }
+            // Highlight the current polygon
+            highlightId = Number(e.features[0].id);
+            map.current!.setFeatureState(
+              { source: 'custom-polygons', id: e.features[0].id },
+              { hover: true }
+            );
+          }
+        });
+
+        // MOUSE LEAVE
+        map.current!.on('mouseleave', 'custom-polygons-layer', () => {
+
+          // Update cursor (back to default)
+          map.current!.getCanvas().style.cursor = '';
+
+          // If polygon is highlighted, un-highlight it
+          if (highlightId !== null) {
+            map.current!.setFeatureState(
+              { source: 'custom-polygons', id: highlightId },
+              { hover: false }
+            );
+            highlightId = null;
+          }
+        });
+      } catch (error) {
+        console.error('Could not build map layers:', error);
+      }
     }
-  }, []);
 
-  return <div id="map" style={{ height: '100vh' }} />;
-};
+    // Call buildMapLayers after the map has loaded
+    map.current.on('load', buildMapLayers);
+  });
 
-export default Map;
+  return (
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
+}
