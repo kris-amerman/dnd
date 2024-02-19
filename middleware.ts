7c6f54c5cae1b7a0424df21c0dd0a9aa43f43730
Kristoffer Amerman <kristofferamerman@kris-mac.local>
2024-02-19 17:27:43 -0500

    testing out middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
 import { auth } from "@/auth";
 
+console.log(`calling MIDDLEWARE`)
+
 export const middleware = auth;
 
-export const config = { matcher: ["/middleware-example"] };
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
