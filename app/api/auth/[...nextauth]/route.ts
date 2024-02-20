6fbb7bf4517f43057f640e1336086b2273b96c82
Kristoffer Amerman <kristofferamerman@kris-mac.local>
2024-02-20 16:50:30 -0500

    handle callback errors

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import { handlers } from "@/auth";
-import { NextRequest, NextResponse} from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const { GET: AuthGET, POST } = handlers;
 export { POST };
@@ -8,18 +8,21 @@ export { POST };
 export async function GET(request: NextRequest) {
   console.log(`route.ts GET ${request.nextUrl.href}`)
 
-  if (request.nextUrl.pathname === '/api/auth/signin') { 
+  if (request.nextUrl.pathname === '/api/auth/signin') {
     // Redirect the user to `/sign-in` page instead of default next-auth page
     return NextResponse.redirect('http://localhost:3000/sign-in');
   }
 
   const urlSearchParams = new URLSearchParams(request.nextUrl.searchParams);
 
-  if (
-    urlSearchParams.get('error') === 'AuthorizedCallbackError' // Access Denied
-  ) {
-    // Redirect the user to `/sign-in` page on unsuccessful sign-in (signIn returned false)
-    return NextResponse.redirect('http://localhost:3000/sign-in');
+  const error = urlSearchParams.get('error')
+  switch (error) {
+    case 'AuthorizedCallbackError':
+      // Redirect the user to `/sign-in` page on unsuccessful sign-in (signIn returned false)
+      return NextResponse.redirect('http://localhost:3000/sign-in');
+    case 'CallbackRouteError':
+      // Redirect the user to `/sign-in` page on unsuccessful sign-in (user denied access to the application)
+      return NextResponse.redirect('http://localhost:3000/sign-in');
   }
 
   const response = await AuthGET(request);
